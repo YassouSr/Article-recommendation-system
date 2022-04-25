9697182718b33213c573efc3f2ba90ba3d155e95
GloryClover <yasmineyaw@outlook.com>
2022-04-25 22:04:21 +0100

    setup basic structure

--- bloc/__init__.py
+++ bloc/__init__.py
@@ -3,16 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '33cb60423f3d6e46effde4efe0c19f63'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:rima1999@localhost:5432/postgres"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+username = "postgres"
+port = "5432"
+db_name = "recommendation"
+db_password = "amina"
+secret_key = "1478df-dfsefdfs-dsfeffdf"
+
+app.config["SECRET_KEY"] = secret_key
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{}:{}@localhost:{}/{}".format(
+    username, db_password, port, db_name
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
+# don't delete this line otherwise it won't run the application
+# because import statements are causing cyclic import (loop) and this line stops it
 from bloc import routes
--- execute.py
+++ execute.py
@@ -1,5 +1,4 @@
 from bloc import app
 
-if __name__ == '__main__':
-	
-	app.run(debug=True , port=8500)
+if __name__ == "__main__":
+    app.run(debug=True)
